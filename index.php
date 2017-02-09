5c50ac24a594ddaef184e9ea4a69c6c3c99173f4
Kristen Nyce <treasureharpy@gmail.com>
2017-02-09 15:49:14 -0500

    fix validation errors

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require('logic.php'); ?>
 <!DOCTYPE html>
 <head>
+<title>Assignment 1</title>
 <link rel="stylesheet" type="text/css" href="/css/a1.css">
 <link href='//fonts.googleapis.com/css?family=Amatica SC' rel='stylesheet'>
 </head>
@@ -8,7 +9,7 @@
 <body>
 <div class="about">
 	<h1>Kristen Nyce</h1>
-	<img src="/images/me.JPG" height="200px" alt="me">
+	<img src="/images/me.JPG" height="200" alt="me">
 	<h1>About Me</h1>
 	<div class="about_text">
 	    <h3><?=$about?></h3>
--- logic.php
+++ logic.php
@@ -8,14 +8,11 @@ $about = "I am an artist currently living in Northern Virginia."
          ." and so am interested in taking this class to gain more formal training in software development and learn best practices.";
          
 //array of quotes
-$quotes = ["When it's over, I want to say: all my life I was a bride married to amazement. 
-                </br>I was the bridegroom, taking the world into my arms. -Mary Oliver",
-		"Empty-handed I entered the world. Barefoot I leave it.
-		</br>My coming, my going 
-		</br>Two simple happenings
-		</br>That got entangled. -Kozan Ichikyo",
-	 	"I found the poems in the fields,
-		And only wrote them down. -John Clare"];
+$quotes = ["When it's over, I want to say: all my life I was a bride married to amazement." 
+             ." I was the bridegroom, taking the world into my arms. -Mary Oliver"
+	   ,"Empty-handed I entered the world. Barefoot I leave it." 
+	     ." My coming, my going two simple happenings that got entangled. -Kozan Ichikyo"
+	   ,"I found the poems in the fields, And only wrote them down. -John Clare"];
 
 //index of random member of quotes array
 $quote_index = array_rand($quotes,1);
